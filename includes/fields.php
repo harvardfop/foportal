c5f3aa478cff66abaab7a52bdb6bb3db1b60fe1d
Jason Hirschhorn <hirschhorn@college.harvard.edu>
2014-02-21 20:34:10 -0500

    added missing files

---
+++ includes/fields.php
@@ -0,0 +1,33 @@
+<?php
+
+    /**
+     * fields.php
+     *
+     * FOPortal
+     * v.0.1
+     *
+     * Global array to store all of the fields.
+     */
+
+    $fields = [
+    	["field" => "first",
+    	"long" => "First Name",
+        "example" => "Millie"],
+    	["field" => "last",
+    	"long" => "Last Name",
+        "example" => "Teplitz"],
+    	["field" => "year",
+    	"long" => "Class Year",
+        "example" => "2018"],
+    	["field" => "at_college",
+    	"long" => "@college Email Address",
+        "example" => "woof"],
+    	["field" => "photo",
+    	"long" => "Profile Picture",
+        "example" => "N/A"],
+		["field" => "house",
+    	"long" => "Residential House or Dorm",
+        "example" => "Currier"],
+    ];
+
+?>
---
+++ includes/houses.php
@@ -0,0 +1,45 @@
+<?php
+
+    /**
+     * hosues.php
+     *
+     * FOPortal
+     * v.0.1
+     *
+     * Global array to store all of the houses.
+     */
+
+    $houses = [
+        "Adams",
+        "Cabot",
+        "Currier",
+        "Dudley",
+        "Dunster",
+        "Eliot",
+        "Kirkland",
+        "Leverett",
+        "Lowell",
+        "Mather",
+        "Pforzheimer",
+        "Quincy",
+        "Winthrop",
+        "Apley Court",
+        "Canaday",
+        "Grays",
+        "Greenough",
+        "Hollis",
+        "Holworthy",
+        "Hurlbut",
+        "Lionel",
+        "Massachusetts Hall",
+        "Matthews",
+        "Mower",
+        "Pennypacker",
+        "Stoughton",
+        "Straus",
+        "Thayer",
+        "Weld",
+        "Wigglesworth"
+    ];
+
+?>
