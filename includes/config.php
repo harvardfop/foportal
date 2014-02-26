95808090af8520219f24b3cef0842b8ab08491c7
Jason Hirschhorn <hirschhorn@college.harvard.edu>
2014-02-25 21:02:12 -0500

    moved session_start()

--- includes/config.php
+++ includes/config.php
@@ -9,6 +9,9 @@
      * Configures pages.
      */
 
+    // enable sessions
+    session_start();
+
     // display errors, warnings, and notices
     ini_set("display_errors", true);
     error_reporting(E_ALL);
@@ -18,9 +21,6 @@
     require("houses.php");
     require("constants.php");
     require("functions.php");
-
-    // enable sessions
-    session_start();
 
     // require authentication for most pages
     if (!preg_match("{(?:login|logout|register)\.php$}", $_SERVER["PHP_SELF"]))
