10fbfb3b8cf0f9879e878389bae33775dd99a923
Jason Hirschhorn <hirschhorn@college.harvard.edu>
2014-03-02 22:36:17 -0500

    updated email address

--- templates/footer.php
+++ templates/footer.php
@@ -1,8 +1,16 @@
            	</div>
 
             <div id="bottom">
+				<script>
+					$(document).ready(function() {
+						var username = "help";
+						var hostname = "foportal.co";
+						var link = username + "@" + hostname ;
+						$("#bottom a").attr("href", "mail" + "to:" + username + "@" + hostname + "?subject=FOPortal%20Feedback");
+					});
+				</script>
                 <p>Feedback for the site? Found a bug? Would like to see a feature in future versions?<br>
-                Please send Jason <a href="mailto:hirschhorn@college.harvard.edu?subject=FOPortal%20Feedback" target="_blank">an email</a>!</p>
+                Please send us <a href="#" target="_blank">an email</a>!</p>
             </div>
 
         </div>
